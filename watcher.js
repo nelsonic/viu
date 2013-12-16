5d814dfd8bc21be8d2df5ff6763df40afbba8d2d
@nelsonic <nodecoder@gmail.com>
2013-12-16 07:55:54 +0000

    playing with fs.watch to notify when file changes are made

---
+++ watcher.js
@@ -0,0 +1,11 @@
+
+var fs = require("fs");
+
+fs.watch(__dirname, function (event, filename) {
+  console.log('event is: ' + event);
+  if (filename) {
+    console.log('filename provided: ' + filename);
+  } else {
+    console.log('filename not provided');
+  }
+});
